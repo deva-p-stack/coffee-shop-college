a5124423beff05a5a875041c2e64cd549ce187f6
deva-p-stack <devap677@gmail.com>
2026-02-15 17:25:10 +0530

    FIX:deployment issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Header from './components/Header';
 import Signup from './components/Signup';
-import ProfileSettings  from './pages/ProfileSettings';
+import ProfileSettings from './pages/ProfileSettings';
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import CoffeeMenu from './pages/Coffeemenu';
 import Cart from './pages/Cart';
@@ -10,33 +10,33 @@ import Reviews from './pages/Reviews';
 import FAQ from './pages/Faq';
 import OffersEvents from './pages/OffersEvents';
 import ContactUs from './pages/ContactUs';
-import AboutUs  from './pages/Aboutus';
+import AboutUs from './pages/Aboutus';
 import Footer from './components/Footer';
 import Homes from './pages/Homes';
 
 function App() {
-    const [darkMode, setDarkMode] = useState(false);
-  return (
-    <>
-      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
-      {/* <Homes />      */}
-       <Signup darkMode={darkMode} />
-      < ProfileSettings />
-      < CoffeeMenu />
-      < Cart />
-      <Reviews />
-      <FAQ />
-      < OffersEvents />
-      < ContactUs />
-      < AboutUs />
-      <Footer />
+  const [darkMode, setDarkMode] = useState(false);
 
-   
-    
-   
-    
-    </>
-  )
+  return (
+    <Router>
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+
+      <Routes>
+        <Route path="/" element={<Homes />} />
+        <Route path="/signup" element={<Signup darkMode={darkMode} />} />
+        <Route path="/profile" element={<ProfileSettings />} />
+        <Route path="/menu" element={<CoffeeMenu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/offers" element={<OffersEvents />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/about" element={<AboutUs />} />
+      </Routes>
+
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
